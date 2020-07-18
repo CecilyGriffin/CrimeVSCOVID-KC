c333c907126710f7be520515d7f61aa79d847e3e
Taniya Chhabra <62070855+tchhabs@users.noreply.github.com>
2020-07-18 08:35:30 -0700

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,14 @@
+CREATE TABLE COVID (
+  id INT PRIMARY KEY,
+  Zip_Code VARCHAR,
+  COVID19_Cases VARCHAR,
+  Crude_Rate_Per_Hundred_Thousand
+);
+
+CREATE TABLE Crime (
+  id INT PRIMARY KEY,
+  Offense VARCHAR,
+  Zip_Code VARCHAR,
+  Gender VARCHAR,
+  Age INT
+);
