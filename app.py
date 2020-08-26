fa22cdbeef49137858fad06f5e13c90060f8d1f2
Taniya Chhabra <chhabra.taniya@gmail.com>
2020-08-26 12:05:06 -0700

    working on flask/gmap

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
 from flask import Flask, render_template
-
-# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+from config import gkey
 import pymongo
 
 # Create an instance of our Flask app.
-app = Flask(__name__)
+#app = Flask(__name__)
+app = Flask(__name__, template_folder=".")
 
-@app.route
+# you can set key as config
+app.config['GOOGLEMAPS_KEY'] = gkey
+
+
+# you can also pass the key here if you prefer
+GoogleMaps(app, key=gkey)
+
+@app.route("/")
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    print(mymap.html)
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    print(sndmap.html)
+    return render_template('example.html', mymap=mymap, sndmap=sndmap)
 
 if __name__ == '__main__':
     app.run(debug=True)
