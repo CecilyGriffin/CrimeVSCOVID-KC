bffe999502be50a6b54b64dfc80db051d6603fde
Mercy Griffin <teachmercy@gmail.com>
2020-08-25 21:41:55 -0500

    html, sql, qpp.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+@app.route
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#TODO:
+# app route 
+# index.html goes in templates folder w any consecutive HTML files 
+# create app route 
